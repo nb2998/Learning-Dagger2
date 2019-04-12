edb3ede9365eb7f06b9b6301d344de95bdb6a6bd
Nishtha Bhatia <nbhatia329@gmail.com>
2019-04-12 23:24:31 +0530

    Update gradle and comments

--- app/src/main/java/com/apps/nb2998/learningDagger2/WheelsModule.java
+++ app/src/main/java/com/apps/nb2998/learningDagger2/WheelsModule.java
@@ -6,20 +6,21 @@ import dagger.Provides;
 @Module  // contribute to object graph via methods
 public class WheelsModule {
 
-    @Provides // should be made static when possible, like not possible when instance variable in the module
+    @Provides
+    // should be made static when possible, like not possible when instance variable in the module .. static means doesn't depend on instance state of the module
     static Rims provideRims() {
         return new Rims();
     }
 
     @Provides
-    static Tyres provideTyres(){
+    static Tyres provideTyres() {  // static improves performance as dagger doesn't have to instantiate the module class
         Tyres tyre = new Tyres();
         tyre.inflate();
         return tyre;
     }
 
     @Provides
-    static Wheels provideWheels(Rims rim, Tyres tyre){
+    static Wheels provideWheels(Rims rim, Tyres tyre) {
         return new Wheels(rim, tyre);
     }
 }
